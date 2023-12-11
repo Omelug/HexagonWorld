c981efa51fa9b0f1c9fd21f374660ce9176c1c8f
Omelug <vitjanecek2@gmail.com>
2023-12-11 22:05:38 +0100

    jython

---
+++ src/main/java/hexaworld/net/Packet.java
@@ -0,0 +1,51 @@
+package hexaworld.net;
+
+import hexaworld.CLog;
+import hexaworld.server.ServerAPI;
+import lombok.Getter;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class Packet {
+  static final CLog log = new CLog(CLog.ConsoleColors.CYAN_BOLD);
+
+  /*public static void send(ServerAPI.Socket socket, byte[] data){
+    try {
+      if (socket == null) {
+        log.error("Socket is null");
+        return;
+      }
+
+      OutputStream out = socket.getOutputStream();
+
+      out.write(data);
+      out.write("\n".getBytes());
+
+    } catch (IOException e) {
+      log.error("error sendDataTCP " + e.getMessage());
+    }
+  }*/
+
+  public enum PacketType {
+    LOGIN,
+    COMMAND,
+    CHAT,
+    TICK,
+    CHAT_UPDATE,
+    CHUNK
+  }
+
+  @Getter
+  PacketType ID;
+  @Getter
+  private byte[] data;
+
+  public Packet(PacketType type, byte[] data) {
+
+  }
+
+}
